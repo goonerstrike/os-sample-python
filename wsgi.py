1a45d90c674cdce27dd14b4b4cf483cdb9ae939e
Ryan Chiera (CTR) <ryan.chiera1.ctr@navy.mil>
2017-07-05 13:21:51 -0700

    change to trigger build again

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!!!, Trigger this build"
+    return "Hello World!!!, Made another change!"
 
 if __name__ == "__main__":
     application.run()
