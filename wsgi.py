e1ea033fc670240f7e238dae8969777d4d8a1147
Ryan Chiera (CTR) <ryan.chiera1.ctr@navy.mil>
2017-07-05 13:08:22 -0700

    adding trigger commit

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!!!__!_!_!"
+    return "Hello World!!!, Trigger"
 
 if __name__ == "__main__":
     application.run()
