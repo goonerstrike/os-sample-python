699ae214b2d7c529fb9b8ffdb67cbda8185a37a9
goonerstrike <goonerstrike@users.noreply.github.com>
2017-12-05 13:57:26 -0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,12 @@ import requests
 
 class TestFlask(unittest.TestCase):
   def test_top_tweets(self):
-    r = requests.get("http://127.0.0.1:5000/top_tweets")
+    r = requests.get("http://127.0.0.1:5000/")
     page_src = r.text
 
     if page_src.find("Hello World") < 0:
-      self.fail("Cannot find hello world")
+      print("BLEH")
+      self.fail("Cannot find Hello World")
 
-
-
+if __name__ == "__main__":
+    unittest.main(warnings='ignore', failfast = True)
