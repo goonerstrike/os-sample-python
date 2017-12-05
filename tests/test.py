870dcd0f8b0599a9d9aabce3f4dda2b56d32a974
Ryan Chiera (CTR) <ryan.chiera1.ctr@navy.mil>
2017-12-05 13:49:22 -0800

    adding test.py

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+import requests
+
+class TestFlask(unittest.TestCase):
+  def test_top_tweets(self):
+    r = requests.get("http://127.0.0.1:5000/top_tweets")
+    page_src = r.text
+
+    if page_src.find("Hello World") < 0:
+      self.fail("Cannot find hello world")
+
